b97b9a8b46969d8530e07108587396153be29ae4
Tim Stephani <timmy.stephani@gmail.com>
2015-11-30 23:54:13 -0600

    added summary to edit page

--- app/models/property.rb
+++ app/models/property.rb
@@ -27,43 +27,40 @@ class Property < ActiveRecord::Base
 
   RESIDENTIAL_DEPRECIATION_YEARS = 27.5
   COMMERCIAL_DEPRECIATION_YEARS = 39
+  CAP_GAIN_TAX_RATE_PERCENT = 15
+  RECAPTURE_RATE = 0.33 # TODO: research this
 
   validates_presence_of :monthly_rental_income, :down_payment_percent, :sale_price, :operating_expenses_percent,
     :vacancy_percent, :loan_interest_percent, :loan_length_years, :sales_commission_percent, :num_years_to_hold,
     :rent_price_increase_percent, :tax_rate_percent
 
-  def noi(year)
-    gross_income(year) - vacancy(year) - operating_expenses(year)
+  def after_tax_cash_flow(year)
+    after_tax_income(year) + depreciation - principal(year)
   end
 
-  def vacancy(year)
-    gross_income(year) * (vacancy_percent / 100)
+  def after_tax_income(year)
+    pre_tax_income(year) - taxes(year)
+  end
+
+  def after_tax_proceeds
+    new_sale_price - sales_commission - cap_gain_tax - recapture - mortgage_balance
   end
 
   def annual_income
     monthly_rental_income * 12 
   end
 
+  def cap_gain_tax
+    (new_sale_price - sales_commission - sale_price) * CAP_GAIN_TAX_RATE_PERCENT / 100
+  end
+
   def cap_rate(year)
     noi(year) / sale_price
   end
 
-  def down_payment
-    sale_price * (down_payment_percent.to_f / 100)
-  end
-
-  def gross_income(year)
-    return annual_income if year == 1
-    return gross_income(year - 1) * (1 + (rent_price_increase_percent.to_f / 100))
-  end
-
   # TODO: rename sale_price to purchase_price
   def cash_on_cash(year)
-    after_tax_cash_flow(year) / down_payment
-  end
-
-  def depreciation
-    depreciable_assets_cost/ RESIDENTIAL_DEPRECIATION_YEARS
+    after_tax_cash_flow(year) / down_payment.to_f
   end
 
   def depreciable_assets_cost
@@ -71,24 +68,29 @@ class Property < ActiveRecord::Base
     sale_price
   end
 
-  def gross_rent_multiplier
-    sale_price / gross_income(1)
+  def depreciation
+    depreciable_assets_cost/ RESIDENTIAL_DEPRECIATION_YEARS
   end
 
-  def pre_tax_income(year)
-    noi(year) - depreciation - interest_expense(year)
+  def down_payment
+    sale_price * (down_payment_percent.to_f / 100)
   end
 
   def estimated_sale_price(year)
     noi(year) / cap_rate(year)
   end
 
-  def operating_expenses(year)
-    gross_income(year) * (operating_expenses_percent / 100)
-  end
-
   def full_address
     address + ', ' + city + ', ' + state + ', ' + zip
+  end
+
+  def gross_income(year)
+    return annual_income if year == 1
+    return gross_income(year - 1) * (1 + (rent_price_increase_percent.to_f / 100))
+  end
+
+  def gross_rent_multiplier
+    sale_price / gross_income(1)
   end
 
   def interest_expense(year)
@@ -119,16 +121,29 @@ class Property < ActiveRecord::Base
     total_interest
   end
 
+  def money_loaned
+    sale_price - down_payment
+  end
+
+  def mortgage_balance
+    money_loaned - principal(num_years_to_hold)
+  end
+
+  # TODO: rename me to sale_price once sale_price is renamed to purchase price
+  def new_sale_price 
+    noi(num_years_to_hold + 1) / cap_rate(num_years_to_hold + 1)
+  end
+
+  def noi(year)
+    gross_income(year) - vacancy(year) - operating_expenses(year)
+  end
+
+  def operating_expenses(year)
+    gross_income(year) * (operating_expenses_percent / 100)
+  end
+
   def pre_tax_income(year)
     noi(year) - depreciation - interest_expense(year)
-  end
-
-  def taxes(year)
-    pre_tax_income(year) * (tax_rate_percent.to_f / 100)
-  end
-
-  def after_tax_income(year)
-    pre_tax_income(year) - taxes(year)
   end
 
   def principal(year)
@@ -158,16 +173,20 @@ class Property < ActiveRecord::Base
     total_principal
   end
 
-  def after_tax_cash_flow(year)
-    after_tax_income(year) + depreciation - principal(year)
+  def recapture
+    depreciation * num_years_to_hold * RECAPTURE_RATE
   end
 
-  def cash_on_cash(year)
-    puts 'after_tax_cash_flow: ' + after_tax_cash_flow(year).to_s
-    puts 'down_payment: ' + down_payment.to_s
-    
-    after_tax_cash_flow(year) / down_payment.to_f
+  def sales_commission
+    new_sale_price * sales_commission_percent / 100
   end
 
+  def taxes(year)
+    pre_tax_income(year) * (tax_rate_percent.to_f / 100)
+  end
+
+  def vacancy(year)
+    gross_income(year) * (vacancy_percent / 100)
+  end
 
 end
